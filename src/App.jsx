d480316e030e8ca0a0544f9cefc486afdb6abd39
Verba Robert <vrobert_1999@yahoo.com>
2023-03-24 18:36:00 +0200

    first version 2023 (with some hardcoded values)

---
+++ src/App.jsx
@@ -0,0 +1,232 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [pricePerHour, setPricePerHour] = useState(0);
+  const [pricePerYear, setPricePerYear] = useState(0);
+  const [pricePerYearInRON, setPricePerYearInRON] = useState(0);
+  const [hoursPerDay, setHoursPerDay] = useState(0);
+  const [hoursPerYear, setHoursPerYear] = useState(0);
+  const [minimumGrossWage, setMinimumGrossWage] = useState(3000);
+  const [netIncome, setNetIncome] = useState(0);
+  const [netIncomeHour, setNetIncomeHour] = useState(0);
+  const [totalTaxes, setTotalTaxes] = useState(0);
+  const [casFlag, setCasFlag] = useState(false);
+  const [cassFlag, setCassFlag] = useState(false);
+  const [cassLowLimitFlag, setCassLowLimitFlag] = useState(false);
+  const [middleLimitFlag, setMiddleLimitFlag] = useState(false);
+  const [casValue, setCasValue] = useState(0);
+  const [cassValue, setCassValue] = useState(0);
+  const [incomeTax, setIncomeTax] = useState(0);
+  const [errorFlag, setErrorFlag] = useState(false);
+  const [textFlag, setTextFlag] = useState(false);
+
+  const numberOfWorkingDays = 261;
+
+  const handleSubmit = () => {
+    setTextFlag(true);
+  };
+
+  const handlePrice = (event) => {
+    const value = event.target.value;
+
+    if (value == "") {
+      setErrorFlag(false);
+    } else if (value.match(/^[0-9]{0,3}[.]?[0-9]{0,6}$/)) {
+      setErrorFlag(false);
+      setPricePerHour(value);
+    } else {
+      setErrorFlag(true);
+    }
+  };
+
+  const handleHours = (event) => {
+    const value = event.target.value;
+
+    if (value == "") {
+      setErrorFlag(false);
+    } else if (!value.match(/^[0-9]{0,2}[.]?[0-9]{0,2}$/) || parseFloat(value) > 12) {
+      setErrorFlag(true);
+    } else {
+      setErrorFlag(false);
+      setHoursPerDay(value);
+    }
+  };
+
+  const percentColors = [
+    { pct: 0.1, color: { r: 0x00, g: 0xff, b: 0 } },
+    { pct: 0.26, color: { r: 0xff, g: 0xff, b: 0 } },
+    { pct: 0.42, color: { r: 0xff, g: 0x00, b: 0 } },
+  ];
+
+  const getColorForPercentage = function (pct) {
+    for (var i = 1; i < percentColors.length - 1; i++) {
+      if (pct < percentColors[i].pct) {
+        break;
+      }
+    }
+    const lower = percentColors[i - 1];
+    const upper = percentColors[i];
+    const range = upper.pct - lower.pct;
+    const rangePct = (pct - lower.pct) / range;
+    const pctLower = 1 - rangePct;
+    const pctUpper = rangePct;
+    const color = {
+      r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+      g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+      b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper),
+    };
+    return "rgb(" + [color.r, color.g, color.b].join(",") + ")";
+  };
+
+  if (document.getElementById("td") != null) {
+    document.getElementById("td").style.backgroundColor = getColorForPercentage(
+      (netIncome / pricePerYearInRON) * -1 + 1
+    );
+    document.getElementById("td").style.color = "black";
+  }
+
+  useEffect(() => {
+    setHoursPerYear(numberOfWorkingDays * hoursPerDay);
+    setPricePerYear(hoursPerYear * pricePerHour);
+    setPricePerYearInRON(pricePerYear * 5);
+    setCasFlag(pricePerYearInRON > minimumGrossWage * 12);
+    setCassFlag(pricePerYearInRON > minimumGrossWage * 6);
+    setMiddleLimitFlag(
+      pricePerYearInRON > minimumGrossWage * 12 &&
+        pricePerYearInRON < minimumGrossWage * 24
+    );
+
+    if (!middleLimitFlag) {
+      setCassLowLimitFlag(
+        pricePerYearInRON > minimumGrossWage * 6 &&
+          pricePerYearInRON < minimumGrossWage * 12
+      );
+    }
+
+    if (!cassFlag) {
+      setCassValue(0);
+      setCasValue(0);
+    } else if (cassLowLimitFlag) {
+      setCassValue(1800);
+      setCasValue(0);
+    } else if (middleLimitFlag) {
+      setCassValue(3600);
+      setCasValue(9000);
+    } else {
+      setCassValue(7200);
+      setCasValue(18000);
+    }
+    setIncomeTax((pricePerYearInRON - casValue - cassValue) * 0.1);
+    setNetIncome(incomeTax * 9);
+    setNetIncomeHour(netIncome / hoursPerYear / 5);
+    setTotalTaxes(Math.round(((netIncome / pricePerYearInRON) * -1 + 1) * 100));
+  });
+
+  return (
+    <div className="App" style={{ display: "flex", flexDirection: "column" }}>
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <label style={{ marginBottom: "10px" }}>
+          Price per hour (€):{" "}
+          <input
+            type="text"
+            name="price-per-hour"
+            onChange={handlePrice}
+            maxLength="9"
+            placeholder="0"
+          />
+        </label>
+        <label style={{ marginBottom: "10px" }}>
+          Hours per day:{" "}
+          <input
+            type="text"
+            name="hours-per-day"
+            onChange={handleHours}
+            maxLength="5"
+            placeholder="0"
+          />
+        </label>
+        {/* <label style={{ marginBottom: "10px" }}>
+          Hours per day:{" "}
+          <input
+            type="text"
+            name="hours-per-day"
+            onChange={handleHours}
+            maxLength="2"
+            placeholder="0"
+          />
+        </label> */}
+      </form>
+      {errorFlag && (
+        <div style={{ color: "red" }}> Wrong input! </div>
+      )}
+      {textFlag ? (
+        <>
+          <table>
+            <tr>
+              <th>HOURS/DAY</th>
+              <th>HOURS/YEAR</th>
+              <th>PRICE/YEAR IN €</th>
+              <th>PRICE/YEAR IN RON</th>
+              {casFlag && <th>CAS 25% IN RON</th>}
+              {cassFlag && <th>CASS 10% IN RON</th>}
+              <th>INCOME TAX 10% IN RON</th>
+              <th>NET INCOME/YEAR IN RON</th>
+              <th>NET INCOME/HOUR IN €</th>
+              <th>TOTAL TAXES %</th>
+            </tr>
+            <tr>
+              <td>{hoursPerDay}</td>
+              <td>{hoursPerYear}</td>
+              <td>{pricePerYear}</td>
+              <td>{pricePerYearInRON}</td>
+              {casFlag && <td>{casValue}</td>}
+              {cassFlag && <td>{cassValue}</td>}
+              <td>{incomeTax}</td>
+              <td>{netIncome}</td>
+              <td>{netIncomeHour}</td>
+              <td id="td">{totalTaxes}</td>
+            </tr>
+          </table>
+          <p style={{ display: "none" }}>
+            LA {hoursPerDay} ORE: {hoursPerYear} ORE LUCRATE =&gt;{" "}
+            {pricePerYear}
+            €/AN ~ {pricePerYearInRON} RON/AN =&gt;{" "}
+            {casFlag && (
+              <span>
+                CAS 25%{" "}
+                {middleLimitFlag ? (
+                  <span>(36000 * 0.25 = 9000)</span>
+                ) : (
+                  <span>(72000 * 0.25 = 18000)</span>
+                )}
+                ;{" "}
+              </span>
+            )}
+            {cassFlag && (
+              <span>
+                CASS 10%{" "}
+                {cassLowLimitFlag ? (
+                  <span>(18000 * 0.1 = 1800)</span>
+                ) : middleLimitFlag ? (
+                  <span>(36000 * 0.1 = 3600)</span>
+                ) : (
+                  <span>(72000 * 0.1 = 7200)</span>
+                )}
+                ;
+              </span>
+            )}{" "}
+            IMPOZIT PE VENIT 10% =&gt; ({pricePerYearInRON}
+            {casFlag && <span> - {casValue}</span>}
+            {cassFlag && <span> - {cassValue}</span>}) * 0.9 = {netIncome}{" "}
+            RON/AN IN MANA ~ {totalTaxes}% TAXE
+          </p>
+        </>
+      ) : (
+        <button onClick={handleSubmit}>Get Taxes</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
